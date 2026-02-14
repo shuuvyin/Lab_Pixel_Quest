d2639f9cadb36976c80ae63b2e18116ce4133799
shuuvyin <silversoulest@gmail.com>
2026-02-14 16:29:12 -0500

    script

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrwipt : MonoBehaviour
+{
+    string variableOne = "Hello";
+    // Start is called before the first frame update
+    void Start()
+    {
+        string variableTwo = "World";
+        Debug.Log(variableOne + variableTwo);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
