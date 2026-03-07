4f333cc6fa4f8d5f783e38ef8af9ff98d866b88e
shuuvyin <silversoulest@gmail.com>
2026-03-07 16:33:07 -0500

    kill briks

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,22 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Scrwipt : MonoBehaviour
 {
-    int var = 3;
-    string variableOne = "Hello";
+    private Rigidbody2D rb;
+    public int speed = 5;
+    public string nextLevel = "Killbrik2";
+    private SpriteRenderer spriteRenderer;
+
+ 
     // Start is called before the first frame update
     void Start()
     {
-        string variableTwo = "World";
-        Debug.Log(variableOne + variableTwo);
-
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            spriteRenderer.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            spriteRenderer.color = Color.green;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            spriteRenderer.color = Color.blue;
+        }
+
+
+
+        /*
         if (Input.GetKeyDown(KeyCode.W))
         {
             transform.position += new Vector3(0, 1, 0);
@@ -26,5 +53,37 @@ public class Scrwipt : MonoBehaviour
             transform.position += new Vector3(0, -1, 0);
 
         }
+        if (Input.GetKeyDown(KeyCode.A))
+            transform.position += new Vector3(-1, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        */
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Death":
+                {
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    break;
+
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+        }
+
+
     }
 }
+    
+
+
+
