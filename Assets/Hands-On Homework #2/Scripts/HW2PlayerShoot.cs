3750beadf4061ede7c70fd23b57ad9dacef5d65f
shuuvyin <silversoulest@gmail.com>
2026-03-13 23:09:07 -0400

    boom boom

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
@@ -2,5 +2,48 @@ using UnityEngine;
 
 public class HW2PlayerShoot : MonoBehaviour
 {
+    public GameObject preFab;
+    public Transform bulletTrash;
+    public Transform bulletSpawn;
 
+    private const float Timer = 0.5f;
+    private float _currentTime = 0.5f;
+    private bool _canShoot = true;
+
+    private void Update()
+    {
+        if (!_canShoot)
+        {
+            _currentTime -= Time.deltaTime;
+
+            if (_currentTime < 0)
+            {
+                _canShoot = true;
+                _currentTime = Timer;
+            }
+        }
+
+        // LEFT CLICK
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        {
+            GameObject bullet = Instantiate(preFab, bulletSpawn.position, Quaternion.identity);
+            bullet.transform.SetParent(bulletTrash);
+
+            _canShoot = false;
+        }
+
+        // RIGHT CLICK (different color)
+        if (Input.GetKeyDown(KeyCode.Mouse1) && _canShoot)
+        {
+            GameObject bullet = Instantiate(preFab, bulletSpawn.position, Quaternion.identity);
+
+            bullet.transform.SetParent(bulletTrash);
+
+            // change color
+            Renderer r = bullet.GetComponent<Renderer>();
+            r.material.color = Color.red;
+
+            _canShoot = false;
+        }
+    }
 }
